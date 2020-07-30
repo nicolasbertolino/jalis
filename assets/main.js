701524ac2e458088df82e4c7b1cf5c735df8ecef
Nicolas Bertolino <bertolinonicolas@gmail.com>
2020-07-30 16:42:23 +0200

    Launching the rocket 1.0

---
+++ assets/main.js
@@ -0,0 +1,86 @@
+// Nav
+gsap.to("nav > *", {
+  duration: 0.5,
+  delay: 1,
+  y: "-50%",
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
+
+// Socials
+gsap.to(".socials > *", {
+  duration: 1,
+  delay: 1.4,
+  x: 0,
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
+
+// Background text
+gsap.to(".bg-text", {
+  duration: 1,
+  delay: 4,
+  opacity: 1,
+  ease: "power3",
+});
+
+// Intro
+gsap.to(".intro > *", {
+  duration: 1,
+  delay: 2,
+  y: 0,
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
+
+// Watch
+gsap.to(".watch > *", {
+  duration: 1,
+  delay: 2.2,
+  y: 0,
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
+
+// Infos
+gsap.to(".infos > *", {
+  duration: 1,
+  delay: 2.4,
+  y: 0,
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
+
+// Cards
+gsap.to(".cards > *", {
+  duration: 1,
+  delay: 2.6,
+  y: 0,
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
+
+// Awards
+gsap.to(".awards > *", {
+  duration: 1,
+  delay: 2.8,
+  y: 0,
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
+
+// Footer
+gsap.to("footer", {
+  duration: 1,
+  delay: 3,
+  opacity: 1,
+  stagger: 0.1,
+  ease: "power3",
+});
